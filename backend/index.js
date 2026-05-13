64dde11c38f9346c5d5869576d067f53ba0b753b
manisha43-dev <manishakumar2507@gmail.com>
2026-05-13 07:31:58 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const app = express();
 const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:3001",
+   " https://ai-audit-n1my.vercel.app/",
     process.env.CLIENT_URL,
 ];
 
